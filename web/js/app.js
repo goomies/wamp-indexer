0d7010e06dbb3209fe73a8c48e7ed3d380bb8450
GOOMIE-PC\Goomie <remy.manescau@gmail.com>
2018-04-18 03:32:23 +0200

    Update - reorganize js files and delete useless function

--- web/js/app.js
+++ web/js/app.js
@@ -1,5 +1,26 @@
+/*--------------------------------------
+    Detact Mobile Browser
+---------------------------------------*/
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+   $('html').addClass('ismobile');
+}
+
 $(window).load(function(){
-    //Welcome Message (not for login page)
+    /* --------------------------------------------------------
+        Page Loader
+     ---------------------------------------------------------*/
+    if(!$('html').hasClass('ismobile')) {
+        if($('.page-loader')[0]) {
+            setTimeout (function () {
+                $('.page-loader').fadeOut();
+            }, 500);
+
+        }
+    }
+
+    /*--------------------------------------
+        Welcome Message
+    ---------------------------------------*/
     function notify(message, type){
         $.growl({
             message: message
@@ -31,6 +52,49 @@ $(window).load(function(){
 
 $(document).ready(function() {
     /*--------------------------------------
+        Header Search
+    ---------------------------------------*/
+    $('body').on('click', '[data-ma-action]', function (e) {
+        e.preventDefault();
+        var action = $(this).data('ma-action');
+        var $this = $(this);
+
+        switch (action) {
+            /* Clear Search */
+            case 'search-clear':
+                /* For mobile only */
+                $('.h-search').removeClass('toggled');
+                /* For all */
+                $('.hs-input').val('');
+                $('.h-search').removeClass('focused');
+                break;
+            /* Open search */
+            case 'search-open':
+                $('.h-search').addClass('toggled');
+                $('.hs-input').focus();
+                break;
+        }
+    });
+    
+    /* --------------------------------------------------------
+        Top Search
+    ----------------------------------------------------------*/
+
+    /* Bring search reset icon when focused */
+    $('body').on('focus', '.hs-input', function(){
+        $('.h-search').addClass('focused');
+    });
+
+    /* Take off reset icon if input length is 0, when blurred */
+    $('body').on('blur', '.hs-input', function(){
+        var x = $(this).val();
+
+        if (!x.length > 0) {
+            $('.h-search').removeClass('focused');
+        }
+    });
+
+    /*--------------------------------------
         display number of Projects
     ---------------------------------------*/
     $("#_wampindexer").removeClass("grid");
@@ -47,7 +111,7 @@ $(document).ready(function() {
     }
 
     /*--------------------------------------
-        Filter Site
+        Filter Site into the grids
     ---------------------------------------*/
     $( "#myInput" ).keyup(function() {
         var input, filter, container, titre, h2, span, i;
@@ -70,44 +134,6 @@ $(document).ready(function() {
     });
 
     /*--------------------------------------
-        Header Color
-    ---------------------------------------*/
-    $('body').on('click', '.hc-trigger', function() {
-        $(this).parent().toggleClass('toggled');
-    });
-
-    $('body').on('click', '.hc-item', function() {
-        var v = $(this).data('ma-header-value');
-
-        $('.hc-item').removeClass('selected');
-        $(this).addClass('selected');
-
-
-        $('body').attr('data-ma-header', v);
-    });
-
-    /*--------------------------------------
-        Animation
-     ---------------------------------------*/
-    $('body').on('click', '.animation-demo .btn', function(){
-        var animation = $(this).text();
-        var cardImg = $(this).closest('.card').find('img');
-        if (animation === "hinge") {
-            animationDuration = 2100;
-        }
-        else {
-            animationDuration = 1200;
-        }
-
-        cardImg.removeAttr('class');
-        cardImg.addClass('animated '+animation);
-
-        setTimeout(function(){
-            cardImg.removeClass(animation);
-        }, animationDuration);
-    });
-
-    /*--------------------------------------
         Animation
      ---------------------------------------*/
      $(".card.animation-demo").hover(
@@ -117,148 +143,4 @@ $(document).ready(function() {
          $(this).find(".animated").removeClass( "pulse" );
        }
      );
-
-
-    /*--------------------------------------
-         Notifications & Dialogs
-     ---------------------------------------*/
-    /*
-     * Notifications
-     */
-    function notify(from, align, icon, type, animIn, animOut){
-        $.growl({
-            icon: icon,
-            title: ' Bootstrap Growl ',
-            message: 'Turning standard Bootstrap alerts into awesome notifications',
-            url: ''
-        },{
-            element: 'body',
-            type: type,
-            allow_dismiss: true,
-            placement: {
-                from: from,
-                align: align
-            },
-            offset: {
-                x: 30,
-                y: 30
-            },
-            spacing: 10,
-            z_index: 1031,
-            delay: 2500,
-            timer: 1000,
-            url_target: '_blank',
-            mouse_over: false,
-            animate: {
-                enter: animIn,
-                exit: animOut
-            },
-            icon_type: 'class',
-            template: '<div data-growl="container" class="alert" role="alert">' +
-            '<button type="button" class="close" data-growl="dismiss">' +
-            '<span aria-hidden="true">&times;</span>' +
-            '<span class="sr-only">Close</span>' +
-            '</button>' +
-            '<span data-growl="icon"></span>' +
-            '<span data-growl="title"></span>' +
-            '<span data-growl="message"></span>' +
-            '<a href="#" data-growl="url"></a>' +
-            '</div>'
-        });
-    };
-
-    $('.notifications > div > .btn').click(function(e){
-        e.preventDefault();
-        var nFrom = $(this).attr('data-from');
-        var nAlign = $(this).attr('data-align');
-        var nIcons = $(this).attr('data-icon');
-        var nType = $(this).attr('data-type');
-        var nAnimIn = $(this).attr('data-animation-in');
-        var nAnimOut = $(this).attr('data-animation-out');
-
-        notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut);
-    });
-
-
-    /*
-     * Dialogs
-     */
-
-    //Basic
-    $('#sa-basic').click(function(){
-        swal("Here's a message!");
-    });
-
-    //A title with a text under
-    $('#sa-title').click(function(){
-        swal("Here's a message!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat, tincidunt vitae ipsum et, pellentesque maximus enim. Mauris eleifend ex semper, lobortis purus sed, pharetra felis")
-    });
-
-    //Success Message
-    $('#sa-success').click(function(){
-        swal("Good job!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem erat, tincidunt vitae ipsum et, pellentesque maximus enim. Mauris eleifend ex semper, lobortis purus sed, pharetra felis", "success")
-    });
-
-    //Warning Message
-    $('#sa-warning').click(function(){
-        swal({
-            title: "Are you sure?",
-            text: "You will not be able to recover this imaginary file!",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!",
-            closeOnConfirm: false
-        }, function(){
-            swal("Deleted!", "Your imaginary file has been deleted.", "success");
-        });
-    });
-
-    //Parameter
-    $('#sa-params').click(function(){
-        swal({
-            title: "Are you sure?",
-            text: "You will not be able to recover this imaginary file!",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!",
-            cancelButtonText: "No, cancel plx!",
-            closeOnConfirm: false,
-            closeOnCancel: false
-        }, function(isConfirm){
-            if (isConfirm) {
-                swal("Deleted!", "Your imaginary file has been deleted.", "success");
-            } else {
-                swal("Cancelled", "Your imaginary file is safe :)", "error");
-            }
-        });
-    });
-
-    //Custom Image
-    $('#sa-image').click(function(){
-        swal({
-            title: "Sweet!",
-            text: "Here's a custom image.",
-            imageUrl: "img/thumbs-up.png"
-        });
-    });
-
-    //Auto Close Timer
-    $('#sa-close').click(function(){
-        swal({
-            title: "Auto close alert!",
-            text: "I will close in 2 seconds.",
-            timer: 2000,
-            showConfirmButton: false
-        });
-    });
-
-    /*--------------------------------------
-        Components
-     ---------------------------------------*/
-    $('body').on('click', '#btn-color-targets > .btn', function(){
-        var color = $(this).data('target-color');
-        $('#modalColor').attr('data-modal-color', color);
-    });
 });
