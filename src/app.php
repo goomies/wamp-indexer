33d5532316ea28345b4e91cad48cd79bfb3a7b39
Goomie <remy.manescau@gmail.com>
2018-07-04 04:00:15 +0200

    fixed - bolosse - ICONS

--- src/app.php
+++ src/app.php
@@ -22,62 +22,15 @@ foreach ($finder as $content) {
     $extension =  pathinfo($contentName, PATHINFO_EXTENSION);
     // Img repository
     $contentImg = $contentName.'/screenshot.png';
-    $contentIcon = $contentName.'/screenshot.png';
-    // Folder - avatar
+
+    // Icons
     if ($extension == "") {
-        if (file_exists($contentImg)) {
-            $contentIcon = $contentName.'/screenshot.png';
-        } else {
-            $contentIcon = "../_wampindexer/web/img/folder.png";
-        }
+        $contentIcon = "../_wampindexer/web/img/folder.png";
     }
-    // Site & Dev - avatar
     elseif ($extension == "fr" || $extension == "com" || $extension == "org" || $extension == "eu" || $extension == "be") {
         $contentIcon = "../_wampindexer/web/img/site.png";
-    } elseif ($extension == "html") {
-        $contentIcon = "../_wampindexer/web/img/html.png";
-    } elseif ($extension == "css") {
-        $contentIcon = "../_wampindexer/web/img/css.png";
-    } elseif ($extension == "js") {
-        $contentIcon = "../_wampindexer/web/img/js.png";
-    } elseif ($extension == "xml") {
-        $contentIcon = "../_wampindexer/web/img/xml.png";
-    }
-    // Picture - avatar
-    elseif ($extension == "jpg" || $extension == "gif" || $extension == "raw") {
-        $contentIcon = "../_wampindexer/web/img/img.png";
-    } elseif ($extension == "png") {
-        $contentIcon = "../_wampindexer/web/img/img.png";
-    } elseif ($extension == "svg") {
-        $contentIcon = "../_wampindexer/web/img/img.png";
-    } elseif ($extension == "pdf") {
-        $contentIcon = "../_wampindexer/web/img/pdf.png";
-    } elseif ($extension == "psd") {
-        $contentIcon = "../_wampindexer/web/img/psd.png";
-    } elseif ($extension == "ai") {
-        $contentIcon = "../_wampindexer/web/img/ai.png";
-    }
-    // Text - avatar
-    elseif ($extension == "txt") {
-        $contentIcon = "../_wampindexer/web/img/txt.png";
-    } elseif ($extension == "docx") {
-        $contentIcon = "../_wampindexer/web/img/docx.png";
-    } elseif ($extension == "xlsx") {
-        $contentIcon = "../_wampindexer/web/img/xlsx.png";
-    } elseif ($extension == "pptx") {
-        $contentIcon = "../_wampindexer/web/img/pptx.png";
-    }
-    // Movie - avatar
-    elseif ($extension == "mov") {
-        $contentIcon = "../_wampindexer/web/img/mov.png";
-    }
-    // Sound - avatar
-    elseif ($extension == "mp3") {
-        $contentIcon = "../_wampindexer/web/img/mp3.png";
-    }
-    // Archive - avatar
-    elseif ($extension == "zip") {
-        $contentIcon = "../_wampindexer/web/img/zip.png";
+    } else {
+        $contentIcon = "../_wampindexer/web/img/".$extension.".png";
     }
 
     // Description
